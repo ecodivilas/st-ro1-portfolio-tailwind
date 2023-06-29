fdefb8ad0352fa474db65128ef88710dbd25c025
ely <eleazar.odivilas01@gmail.com>
2023-06-29 23:39:52 +0800

    Initialize project portfolio with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Home from "../src/components/Home";
+import About from "../src/components/About";
+import Skills from "../src/components/Skills";
+import Experience from "../src/components/Experience";
+import Projects from "../src/components/Projects";
+import Contact from "../src/components/Contact";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../assets/customize.css";
+
+function Home() {
+  return (
+    <>
+      <div className="w-full bg-slate-950 text-white h-screen flex justify-center items-center">
+        Home
+      </div>
+
+      <div class="pure-css">Flexbox</div>
+
+      <button className="bg-amber-900 text"></button>
+    </>
+  );
+}
+
+export default Home;
